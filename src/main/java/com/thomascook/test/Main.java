418a4cdc05e8b36333e9cf24b7e68be1114d0877
Domingo Gomez <domingo.gomez@thomascookonline.com>
2014-07-24 17:28:52 +0100

    Initial commit

---
+++ src/main/java/com/thomascook/test/Main.java
@@ -0,0 +1,33 @@
+package com.thomascook.test;
+
+import java.io.IOException;
+import java.net.URL;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+
+public class Main {
+
+	//private static Logger LOGGER = LoggerFactory.getLogger(Main.class);
+	private static Logger LOGGER = LoggerFactory.getLogger("AVAIL_RQ");
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+
+			URL url = Resources.getResource("logs/example1.log");
+			String text = Resources.toString(url, Charsets.UTF_8);
+
+			LOGGER.info(text);
+			
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
